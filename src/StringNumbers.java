2850e9991b53c4d0bbb4d3d7c1ef96adf17cd36c
zsaty00 <satylgan.dev@gmail.com>
2023-10-25 22:53:20 -0500

    Test

--- src/StringNumbers.java
+++ src/StringNumbers.java
@@ -1,5 +1,8 @@
 public class StringNumbers {
     public static void main(String[] args) {
         System.out.println("tis is one more change");
+        System.out.println("tis is one more change");
+        System.out.println("tis is one more change");
+        System.out.println("tis is one more change");
     }
 }
