5011a4379ce4b73fb295c802312447cf8c9748d1
zsaty00 <79534499+zsaty00@users.noreply.github.com>
2023-10-26 22:00:45 -0500

    Update GitConflictResolve.java

--- src/GitConflictResolve.java
+++ src/GitConflictResolve.java
@@ -1,8 +1,8 @@
 public class GitConflictResolve {
     public static void main(String[] args) {
 
-        System.out.print(" Hello, Icahnged your code in git ");
-        String str = "I am student one";
+        System.out.print(" I am offline, I'll back after 1 hour. ");
+        //String str = "I am student one";
 
 
     }
