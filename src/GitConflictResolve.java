2c4455d3e1381ab35ade2681adba94928eb83e4e
zsaty00 <satylgan.dev@gmail.com>
2023-10-26 21:21:42 -0500

    Initial version

--- src/GitConflictResolve.java
+++ src/GitConflictResolve.java
@@ -1,6 +1,7 @@
 public class GitConflictResolve {
     public static void main(String[] args) {
 
+        System.out.println(" Codewiser Code java Python ");
         String str = "I am student one";
 
 
