d80b117a3cdd27803b8b1ff5d9261b7b4abdf7d4
zsaty00 <satylgan.dev@gmail.com>
2023-10-26 22:09:42 -0500

    comment

--- src/GitConflictResolve.java
+++ src/GitConflictResolve.java
@@ -1,6 +1,9 @@
 public class GitConflictResolve {
     public static void main(String[] args) {
 
+        int salary = 125_000;
+        System.out.println("Qa Salary is "+salary);
+        String str = "I am student one";
         System.out.print(" I am offline, I'll back after 1 hour. ");
         //String str = "I am student one";
 
