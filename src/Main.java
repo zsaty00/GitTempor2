e281e9eae82a7a96d8d1d31da6ebdcefab12d549
Zhaksybek Satylgan <jjfamily@Zhaksybeks-MacBook-Pro.local>
2023-10-26 19:05:16 -0500

    I changed code from mac

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        String str = "I changed this lane" ;
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
+
+        for (int i=0; i<=10; i++){
+            System.out.println(i);
+        }
     }
 }
